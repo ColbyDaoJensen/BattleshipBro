7502b4ad3d8e03b886e55acac2614f81e03b6fcd
Colby DaoJensen <105410293+ColbyDaoJensen@users.noreply.github.com>
2022-05-24 19:43:17 +0000

    Added the imports.

--- Main.java
+++ Main.java
@@ -1,3 +1,8 @@
+import static java.lang.System.*;
+import java.util.Scanner;
+import java.io.IOException;
+import java.io.File;
+
 class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
