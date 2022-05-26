1369b65f0134d0b47307ea39b5c873a007464b4c
Colby DaoJensen <105410293+ColbyDaoJensen@users.noreply.github.com>
2022-05-26 19:39:28 +0000

    Set up variables for Game.

--- GameBoard.java
+++ GameBoard.java
@@ -3,3 +3,16 @@ import java.util.Scanner;
 import java.io.IOException;
 import java.io.File;
 
+public class Game
+{
+  private Board[] playerBoards;
+  private Board curPlayer, curOpponent;
+  private Scanner keyboard;
+  private Scanner file;
+  private boolean skipSetup;
+  private boolean gameOver;
+  
+  public static final String CLEARSCREEN = "\033[H\033[2J";
+  public static final String[] shipNames = {"Carrier", "Battleship", "Cruiser", "Submarine", "Destroyer"};
+  public static final int[] shipSizes = {5, 4, 3, 3, 2};
+
