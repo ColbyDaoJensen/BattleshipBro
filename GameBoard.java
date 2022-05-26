9aa6669c239540389d759d967be8189c74e6285f
Colby DaoJensen <105410293+ColbyDaoJensen@users.noreply.github.com>
2022-05-26 19:44:08 +0000

    Set up Gameboard IO Exceptions

--- GameBoard.java
+++ GameBoard.java
@@ -5,14 +5,27 @@ import java.io.File;
 
 public class Game
 {
+  //Variables for ships and players.
   private Board[] playerBoards;
   private Board curPlayer, curOpponent;
   private Scanner keyboard;
   private Scanner file;
   private boolean skipSetup;
   private boolean gameOver;
-  
+
+  //Sets up ships in the game for players
   public static final String CLEARSCREEN = "\033[H\033[2J";
   public static final String[] shipNames = {"Carrier", "Battleship", "Cruiser", "Submarine", "Destroyer"};
   public static final int[] shipSizes = {5, 4, 3, 3, 2};
+
+  //Allows board to be playable
+  public Game() throws IOException
+  {
+    playerBoards = new Board[]{new Board(), new Board()};
+    curPlayer = playerBoards[0];
+    curOpponent = playerBoards[1];
+    keyboard = new Scanner(System.in);
+    skipSetup = false;
+    gameOver = false;
+  }
 
