ca0816fcdbb76101367e0e73cfb3667200975a6c
Colby DaoJensen <105410293+ColbyDaoJensen@users.noreply.github.com>
2022-05-26 19:20:52 +0000

    Added code for accessing new game. Game can clear screen now.

--- Main.java
+++ Main.java
@@ -1,3 +1,21 @@
 import java.io.IOException;
 import static java.lang.System.*;
 
+public class Main 
+{
+  public static void main(String[] args) throws IOException
+  {
+    try
+    {
+      Game game = new Game();
+      game.play();
+    }
+    catch(IOException e)
+    {
+      out.println(Game.CLEARSCREEN);
+      out.println("Terminated due to IOException:\n");
+      out.println(e.getMessage());
+    }
+  }
+}
+
